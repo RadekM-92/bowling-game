8e4a2b8ed558f890d31f6a3e562535d4a4680867
Radoslaw Marcinowski <rm92.git@gmail.com>
2025-07-06 23:59:24 +0200

    Added variables to heap area

--- bowling-game.hpp
+++ bowling-game.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstddef>
+#include <memory>
 #include <stdexcept>
 #include <vector>
 
@@ -7,19 +8,27 @@ struct PinsKoRangeError : public std::range_error {
     PinsKoRangeError(std::string const&);
 };
 
+struct Counters_t {
+    size_t rollCounter;
+    size_t frameCounter;
+    size_t totalScore;
+    size_t pinsLeft;
+    std::pair<size_t, size_t> knockedDownPinsPerFrame;
+    bool gameEnd;
+};
+
+struct Parameters_t {
+    size_t pinsMaxInFrame;
+    size_t framesMaxWithoutBonus;
+    size_t framesMaxWithBonus;
+    size_t rollsInFrameMax;
+};
+
 class BowlingGame {
 private:
-    size_t rollCounter_ = 0U;
-    size_t frameCounter_ = 0U;
-    size_t totalScore_ = 0U;
-    bool gameEnd_ = false;
-    const size_t pinsMaxInFrame = 10U;
-    const size_t framesMaxWithoutBonus_ = 10U;
-    const size_t framesMaxWithBonus_ = 12U;
-    const size_t rollsInFrameMax_ = 2U;
+    std::unique_ptr<Counters_t> Counters_ { new Counters_t { 0U, 0U, 0U, 10U, { 0U, 0U }, false } };
+    std::unique_ptr<Parameters_t> Parameters_ { new Parameters_t { 10U, 10U, 12U, 2U } };
     std::vector<std::pair<size_t, size_t>> knockedDownPins_;
-    std::pair<size_t, size_t> knockedDownPinsPerFrame_ { 0U, 0U };
-    size_t pinsLeft_ = 10U;
 
     bool isStrike(const std::pair<size_t, size_t>&);
     bool isSpare(const std::pair<size_t, size_t>&);
