b19185c216bd89c8757e70ce0e38b73bc78f2e29
Radoslaw Marcinowski <rm92.git@gmail.com>
2025-08-08 23:01:05 +0200

    Added game over status tests

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,79 @@
 #include <gtest/gtest.h>
+#include <vector>
+#include "bowling-game.hpp"
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("world", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+TEST(BowlingGame, GameIsOverAfterMaxRollsWithoutBonusFrames) {
+    size_t rollsMax = 20U;
+    BowlingGame game;
+
+    for(auto rolls=0; rolls<rollsMax; ++rolls) {
+        game.roll(2);
+    }
+    
+    EXPECT_TRUE(game.getGameEnd());
+}
+
+TEST(BowlingGame, GameIsNotOverBeforeMaxRollsWithoutBonusFrames) {
+    size_t rollsMax = 20U;
+    BowlingGame game;
+
+    for(auto rolls=0; rolls<rollsMax-1; ++rolls) {
+        game.roll(2);
+    }
+    
+    EXPECT_FALSE(game.getGameEnd());
+}
+
+TEST(BowlingGame, GameIsOverAfterStrikeInLastFrame) {
+    size_t rollsMax = 20U;
+    BowlingGame game;
+
+    for(auto rolls=0; rolls<rollsMax-2; ++rolls) {
+        game.roll(2);
+    }
+    game.roll(10);
+    game.roll(2);
+    game.roll(2);
+
+    EXPECT_TRUE(game.getGameEnd());
+}
+
+TEST(BowlingGame, GameIsNotOverAfterStrikeInLastFrame) {
+    size_t rollsMax = 20U;
+    BowlingGame game;
+
+    for(auto rolls=0; rolls<rollsMax-2; ++rolls) {
+        game.roll(2);
+    }
+    game.roll(10);
+    game.roll(2);
+
+    EXPECT_FALSE(game.getGameEnd());
+}
+
+TEST(BowlingGame, GameIsOverAfterSpareInLastFrame) {
+    size_t rollsMax = 20U;
+    BowlingGame game;
+
+    for(auto rolls=0; rolls<rollsMax-2; ++rolls) {
+        game.roll(2);
+    }
+    game.roll(6);
+    game.roll(4);
+    game.roll(2);
+
+    EXPECT_TRUE(game.getGameEnd());
+}
+
+TEST(BowlingGame, GameIsNotOverAfterSpareInLastFrame) {
+    size_t rollsMax = 20U;
+    BowlingGame game;
+
+    for(auto rolls=0; rolls<rollsMax-2; ++rolls) {
+        game.roll(2);
+    }
+    game.roll(6);
+    game.roll(4);
+
+    EXPECT_FALSE(game.getGameEnd());
 }
